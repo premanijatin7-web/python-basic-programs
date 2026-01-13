5ef18660067260998fae4acafcc22438dbcc57b2
Jatin Premani <premanijatin7@gmail.com>
2026-01-13 12:57:50 +0530

    Add Fibonacci series program in Python

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+# Python Program for Fibonacci Series :
+
+n = int(input("Enter number of terms :"))
+        
+a = 0
+b = 1
+
+print("Fibonacci Series :")
+
+for i in range(n) :
+   print(a, end = " ")
+  a,b = b, a+b
+
