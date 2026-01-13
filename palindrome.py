e0c2b095ac76269563cb1f6790f88614cd95bec0
Jatin Premani <premanijatin7@gmail.com>
2026-01-13 14:35:44 +0530

    Add palindrome checker program

---
+++ palindrome.py
@@ -0,0 +1,9 @@
+# Python Program for checking whether a value is palindrome or not :
+
+value = input("Enter a value :")
+if value == value[::-1] :
+  print(value, "is a Palindrome")
+else :
+  print(value, "is not a Palindrome")
+
+
